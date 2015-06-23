63df4e424bbcbdc956b4dc12cd3e4ee7165a50cc
Sat Mandir Khalsa <smkhalsa@gmail.com>
2015-06-22 20:06:32 -0700

    Add bcrypt encryption to passwords

--- db/index.js
+++ db/index.js
@@ -2,20 +2,22 @@ var pg = require('pg');
 var Sequelize = require('sequelize');
 
 var sequelize = new Sequelize('smkhalsa', 'smkhalsa', null, {
-    host: 'localhost',
-    port: 5432,
-    dialect: 'postgres',
-    pool: {
-        max: 5,
-        min: 0,
-        idle: 10000
-    }
+  host: 'localhost',
+  port: 5432,
+  dialect: 'postgres',
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  }
 });
 
 var Users = sequelize.define('users', {
-    email: Sequelize.STRING
+  email: Sequelize.STRING,
+  password: Sequelize.STRING
 });
 
-Users.sync();
+// TODO: remove the force true when adding to production
+Users.sync({force:true});
 
 exports.Users = Users;
